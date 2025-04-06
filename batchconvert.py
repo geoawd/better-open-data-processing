a996bb418537bd98bd1509e6428e4177196e58f4
Alex <119129964+geoawd@users.noreply.github.com>
2025-04-06 10:05:15 +0100

    Add files via upload

---
+++ batchconvert.py
@@ -0,0 +1,18 @@
+import os
+from osgeo import gdal
+
+input_dir = r'lidar/'
+output_dir = r'newlidar/'
+
+for f in os.listdir(input_dir):
+    if f.endswith('.tif'):
+        input_file = os.path.join(input_dir, f)
+        output_file = os.path.join(output_dir, f)
+        
+        translate_options = gdal.TranslateOptions(
+            format='COG',
+            creationOptions=["COMPRESS=DEFLATE", "NUM_THREADS=ALL_CPUS", "STATISTICS=Yes"],
+            outputSRS='EPSG:29902'
+        )
+        
+        gdal.Translate(output_file, input_file, options=translate_options)
